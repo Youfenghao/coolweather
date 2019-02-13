ef305e5162fa74ddff7c16e4e9d427325efe70d7
Mike <2036656973@qq.com>
2019-02-13 11:05:48 +0800

    First submit

---
+++ app/src/main/java/com/example/coolweather/gson/AQI.java
@@ -0,0 +1,18 @@
+package com.example.coolweather.gson;
+
+/**
+ * Created by 米 on 2019/1/31.
+ */
+
+public class AQI {
+
+
+    public AQICity city;
+
+    public class AQICity {
+
+        public String aqi;
+
+        public String PM25;
+    }
+}
